43424b33a35ce4550a33cd0211543158de6cd78d
henryk1229 <henryk1229@gmail.com>
2024-01-08 09:36:10 -0500

    feat: update redirect

--- .netlify/functions-internal/sveltekit-render.mjs
+++ .netlify/functions-internal/sveltekit-render.mjs
@@ -12,7 +12,7 @@ return {
 	assets: new Set(["favicon.png","hk-icon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.f89cb48e.js","app":"_app/immutable/entry/app.c0eb1884.js","imports":["_app/immutable/entry/start.f89cb48e.js","_app/immutable/chunks/scheduler.89b69e6b.js","_app/immutable/chunks/singletons.91e20de8.js","_app/immutable/entry/app.c0eb1884.js","_app/immutable/chunks/scheduler.89b69e6b.js","_app/immutable/chunks/index.86b91312.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.d79e2cc2.js","app":"_app/immutable/entry/app.485bd6b9.js","imports":["_app/immutable/entry/start.d79e2cc2.js","_app/immutable/chunks/scheduler.89b69e6b.js","_app/immutable/chunks/singletons.9bd0936b.js","_app/immutable/entry/app.485bd6b9.js","_app/immutable/chunks/scheduler.89b69e6b.js","_app/immutable/chunks/index.86b91312.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('../server/nodes/0.js')),
 			__memo(() => import('../server/nodes/1.js')),
