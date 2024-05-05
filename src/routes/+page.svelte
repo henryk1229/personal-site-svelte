22977a165aedc26709bec0755e6cc91a5e91334d
henryk1229 <henryk1229@gmail.com>
2024-05-05 17:14:19 -0400

    feat: initial Navbar component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,13 @@
 <script>
 	import Section from './Section.svelte';
+	import NavBar from './NavBar.svelte';
 </script>
 
 <div class="wrapper">
 	<div class="page-container">
 		<div class="page-container-inner">
 			<h1 class="page-title"><span class="page-title-main">Henry Koehler</span></h1>
-			<div class="nav">
-				<div>
-					<span class="nav-item nav-current">Article</span>
-					<span class="nav-item">Talk</span>
-				</div>
-				<div>
-					<span class="nav-item nav-current">Read</span>
-					<span class="nav-item">Edit</span>
-					<span class="nav-item">View history</span>
-					<span class="nav-item">Tools</span>
-				</div>
-			</div>
+			<NavBar />
 			<div class="title-sub">Not from Wikipedia, the free encyclopedia</div>
 			<div class="body-content">
 				<div>
@@ -80,29 +70,6 @@
 		line-height: 1.375;
 		font-weight: normal;
 		border-bottom: 1px solid #a2a9b1;
-	}
-
-	.nav {
-		display: flex;
-		justify-content: space-between;
-		font-size: 0.875rem;
-		box-shadow: 0 1px #c8ccd1;
-		/* margin-bottom: 1px; */
-		/* border-bottom: 1px solid #a2a9b1; */
-		color: #36c;
-
-		padding: 12px 0 7px 0;
-		margin-bottom: -1px;
-	}
-
-	.nav-current {
-		color: #202122;
-		border-bottom: 2px solid;
-	}
-
-	.nav-item {
-		cursor: pointer;
-		margin: 0 8px;
 	}
 
 	.page-container {
---
+++ src/routes/NavBar.svelte
@@ -0,0 +1,36 @@
+<div class="nav">
+	<div>
+		<span class="nav-item nav-current">Article</span>
+		<span class="nav-item">Talk</span>
+	</div>
+	<div>
+		<span class="nav-item nav-current">Read</span>
+		<span class="nav-item">Edit</span>
+		<span class="nav-item">View history</span>
+		<span class="nav-item">Tools</span>
+	</div>
+</div>
+
+<style>
+	.nav {
+		display: flex;
+		justify-content: space-between;
+		font-size: 0.875rem;
+		box-shadow: 0 1px #c8ccd1;
+		/* margin-bottom: 1px; */
+		/* border-bottom: 1px solid #a2a9b1; */
+		color: #36c;
+
+		padding: 12px 0 7px 0;
+		margin-bottom: -1px;
+	}
+
+	.nav-current {
+		color: #202122;
+		border-bottom: 2px solid;
+	}
+
+	.nav-item {
+		margin: 0 8px;
+	}
+</style>
