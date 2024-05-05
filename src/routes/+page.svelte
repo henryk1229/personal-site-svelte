2ed7073c0f6018b2025ffdcba63ffefc620e0514
henryk1229 <henryk1229@gmail.com>
2024-05-05 16:55:39 -0400

    feat: wip, initial styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,61 @@
+<script>
+	import Section from './Section.svelte';
+</script>
+
 <div class="wrapper">
-	<h1>Hi, I'm Henry</h1>
-	<svg style="height: 4px" xmlns="http://www.w3.org/2000/svg">
-		<line x1="0" y1="0" x2="200" y2="0" stroke="black" stroke-width="2px" />
-	</svg>
-	<div>
-		<div>
-			I'm a software engineer working in the <a href="https://www.politicsrewired.com/"
-				>political tech space</a
-			>.
+	<div class="page-container">
+		<div class="page-container-inner">
+			<h1 class="page-title"><span class="page-title-main">Henry Koehler</span></h1>
+			<div class="nav">
+				<div>
+					<span class="nav-item nav-current">Article</span>
+					<span class="nav-item">Talk</span>
+				</div>
+				<div>
+					<span class="nav-item nav-current">Read</span>
+					<span class="nav-item">Edit</span>
+					<span class="nav-item">View history</span>
+					<span class="nav-item">Tools</span>
+				</div>
+			</div>
+			<div class="title-sub">Not from Wikipedia, the free encyclopedia</div>
+			<div class="body-content">
+				<div>
+					I'm a software engineer working in the <a href="https://www.politicsrewired.com/"
+						>political tech space</a
+					>.
+				</div>
+				<div>
+					I principally work with:
+					<ul>
+						<li>Typescript</li>
+						<li>React</li>
+						<li>GraphQL</li>
+						<li>Node</li>
+						<li>PostgreSQL</li>
+						<li>Rust</li>
+					</ul>
+				</div>
+				<div>
+					In my spare time, I like building <a href="https://hhk.nyc/wordsalad">word games</a>
+					and
+					<a
+						href="https://open.spotify.com/artist/5BN5Uywy6N8nWwmIB7Jr7A?si=KM2GxzfzTn61g3ncSZMOAg"
+						target="_blank">making music</a
+					>
+				</div>
+				<h3>Get in touch</h3>
+				<ul>
+					<li>
+						You can find me on <a href="https://www.linkedin.com/in/henry-koehler-a73066176/"
+							>linkedin</a
+						>, follow me on <a href="https://github.com/henryk1229">github</a>, or reach out to me
+						at
+						<a href="mailto:henryk1229@gmail.coms">henryk1229@gmail.com</a>
+					</li>
+				</ul>
+			</div>
 		</div>
-		<div>
-			I principally work with:
-			<ul>
-				<li>Typescript</li>
-				<li>React</li>
-				<li>GraphQL</li>
-				<li>Node</li>
-				<li>PostgreSQL</li>
-				<li>Rust</li>
-			</ul>
-		</div>
-		<div>
-			In my spare time, I like building <a href="https://hhk.nyc/wordsalad">word games</a>
-			and
-			<a
-				href="https://open.spotify.com/artist/5BN5Uywy6N8nWwmIB7Jr7A?si=KM2GxzfzTn61g3ncSZMOAg"
-				target="_blank">making music</a
-			>
-		</div>
-		<h3>Get in touch</h3>
-		<ul>
-			<li>
-				You can find me on <a href="https://www.linkedin.com/in/henry-koehler-a73066176/"
-					>linkedin</a
-				>, follow me on <a href="https://github.com/henryk1229">github</a>, or reach out to me at
-				<a href="mailto:henryk1229@gmail.coms">henryk1229@gmail.com</a>
-			</li>
-		</ul>
 	</div>
 </div>
 
@@ -51,17 +70,75 @@
 			U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 	}
 
-	h1 {
-		margin-bottom: 4px;
-	}
-	div {
-		margin: 12px 0px;
-	}
-	li {
-		margin: 8px;
-	}
 	.wrapper {
-		font-family: Gelasio;
-		padding: 64px 128px;
+		font-family: sans-serif;
+	}
+
+	h1 {
+		font-size: 1.8em;
+		font-family: 'Linux Libertine', 'Georgia', 'Times', 'Source Serif Pro', serif;
+		line-height: 1.375;
+		font-weight: normal;
+		border-bottom: 1px solid #a2a9b1;
+	}
+
+	.nav {
+		display: flex;
+		justify-content: space-between;
+		font-size: 0.875rem;
+		box-shadow: 0 1px #c8ccd1;
+		/* margin-bottom: 1px; */
+		/* border-bottom: 1px solid #a2a9b1; */
+		color: #36c;
+
+		padding: 12px 0 7px 0;
+		margin-bottom: -1px;
+	}
+
+	.nav-current {
+		color: #202122;
+		border-bottom: 2px solid;
+	}
+
+	.nav-item {
+		cursor: pointer;
+		margin: 0 8px;
+	}
+
+	.page-container {
+		min-width: 31.25em;
+		max-width: 99.75rem;
+		box-sizing: border-box;
+		position: relative;
+		z-index: 0;
+		margin: 0 auto;
+		padding-top: 0.05px;
+		padding-left: 1.5rem;
+		padding-right: 1.5rem;
+		background-color: #fff;
+		color: #202122;
+		/* TODO - media queries? */
+		padding-left: 2.75rem;
+		padding-right: 2.75rem;
+	}
+
+	.page-title {
+		margin-bottom: 0;
+	}
+
+	.page-container-inner {
+		/* TODO */
+	}
+
+	.body-content {
+		/* TODO */
+	}
+
+	.page-title-main {
+	}
+
+	.title-sub {
+		margin-top: 8px;
+		font-size: 0.875rem;
 	}
 </style>
