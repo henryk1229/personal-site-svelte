43ffcd166e8e3c1f0917e48861ba14b90cfd4d3f
henryk1229 <henryk1229@gmail.com>
2024-05-05 19:59:26 -0400

    feat: initial sidebar

---
+++ src/routes/Sidebar.svelte
@@ -0,0 +1,39 @@
+<div class="sidebar">
+	<div class="header">Contents</div>
+	<ul>
+		<li>
+			<a href="#Personal_life"> Personal life </a>
+		</li>
+		<li>
+			<a href="#Engineering"> Engineering </a>
+		</li>
+		<li>
+			<a href="#Hobbies">Hobbies</a>
+		</li>
+		<li>
+			<a href="#External_links">External links</a>
+		</li>
+	</ul>
+</div>
+
+<style>
+	.header {
+		padding-bottom: 6px;
+		margin-bottom: 6px;
+		border-bottom: 1px solid #eaecf0;
+		line-height: 1.6;
+		font-weight: bold;
+	}
+	ul {
+		list-style: none;
+		padding: 0;
+	}
+	li {
+		padding: 6px 0px;
+	}
+	a {
+		font-size: 0.875rem;
+		line-height: 1.5;
+		color: #36c;
+	}
+</style>
