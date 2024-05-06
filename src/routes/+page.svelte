8c07f0ff446faef94d53bd31b0724553ab92cb85
henryk1229 <henryk1229@gmail.com>
2024-05-05 20:00:20 -0400

    feat: wip, stylinig

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,77 @@
 <script>
 	import Section from './Section.svelte';
 	import NavBar from './NavBar.svelte';
+	import Sidebar from './Sidebar.svelte';
 </script>
 
 <div class="wrapper">
 	<div class="page-container">
 		<div class="page-container-inner">
-			<h1 class="page-title"><span class="page-title-main">Henry Koehler</span></h1>
-			<NavBar />
-			<div class="title-sub">Not from Wikipedia, the free encyclopedia</div>
-			<div class="body-content">
-				<div>
+			<div class="sider-container">
+				<Sidebar />
+			</div>
+			<div>
+				<h1 class="page-title">Henry Koehler</h1>
+				<NavBar />
+				<div class="title-sub">
+					<span style="font-style:italic">Not</span> from Wikipedia, the free encyclopedia
+				</div>
+				<div class="body-content">
+					<p>
+						<span style="font-weight:bold">Henry Koehler</span> is an American software engineer and
+						musician.
+					</p>
+					<Section>
+						<span slot="title">Personal life</span>
+						<span slot="contents"
+							>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+							the industry's standard dummy text ever since the 1500s, when an unknown printer took
+							a galley of type and scrambled it to make a type specimen book. It has survived not
+							only five centuries, but also the leap into electronic typesetting, remaining
+							essentially unchanged. It was popularised in the 1960s with the release of Letraset
+							sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+							software like Aldus PageMaker including versions of Lorem Ipsum.</span
+						>
+					</Section>
+					<Section>
+						<span slot="title">Engineering</span>
+						<span slot="contents"
+							>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+							the industry's standard dummy text ever since the 1500s, when an unknown printer took
+							a galley of type and scrambled it to make a type specimen book. It has survived not
+							only five centuries, but also the leap into electronic typesetting, remaining
+							essentially unchanged. It was popularised in the 1960s with the release of Letraset
+							sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+							software like Aldus PageMaker including versions of Lorem Ipsum.</span
+						>
+					</Section>
+					<Section>
+						<span slot="title">Hobbies</span>
+						<span slot="contents"
+							>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+							the industry's standard dummy text ever since the 1500s, when an unknown printer took
+							a galley of type and scrambled it to make a type specimen book. It has survived not
+							only five centuries, but also the leap into electronic typesetting, remaining
+							essentially unchanged. It was popularised in the 1960s with the release of Letraset
+							sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+							software like Aldus PageMaker including versions of Lorem Ipsum.</span
+						>
+					</Section>
+					<Section>
+						<span slot="title">External links</span>
+						<span slot="list">
+							<li>
+								<a href="https://www.linkedin.com/in/henry-koehler-a73066176/">Linkedin</a>
+							</li>
+							<li>
+								<a href="https://github.com/henryk1229">Github</a>
+							</li>
+							<li>
+								<a href="mailto:henryk1229@gmail.coms">Gmail</a>
+							</li>
+						</span>
+					</Section>
+					<!-- <div>
 					I'm a software engineer working in the <a href="https://www.politicsrewired.com/"
 						>political tech space</a
 					>.
@@ -33,33 +94,14 @@
 						href="https://open.spotify.com/artist/5BN5Uywy6N8nWwmIB7Jr7A?si=KM2GxzfzTn61g3ncSZMOAg"
 						target="_blank">making music</a
 					>
+				</div> -->
 				</div>
-				<h3>Get in touch</h3>
-				<ul>
-					<li>
-						You can find me on <a href="https://www.linkedin.com/in/henry-koehler-a73066176/"
-							>linkedin</a
-						>, follow me on <a href="https://github.com/henryk1229">github</a>, or reach out to me
-						at
-						<a href="mailto:henryk1229@gmail.coms">henryk1229@gmail.com</a>
-					</li>
-				</ul>
 			</div>
 		</div>
 	</div>
 </div>
 
 <style>
-	@font-face {
-		font-family: 'Gelasio';
-		font-style: normal;
-		font-weight: 400;
-		src: local('Gelasio Regular'), local('Gelasio-Regular'),
-			url(https://fonts.gstatic.com/s/gelasio/v1/cIf9MaFfvUQxTTqS9C6hYQ.woff2) format('woff2');
-		unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
-			U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-	}
-
 	.wrapper {
 		font-family: sans-serif;
 	}
@@ -72,29 +114,42 @@
 		border-bottom: 1px solid #a2a9b1;
 	}
 
+	p {
+		font-size: 14px;
+		line-height: 1.5;
+		margin: 7px 0px 0px;
+		padding: 0px 0px 7px;
+	}
+
+	li {
+		margin-bottom: 0.3em;
+	}
+
 	.page-container {
 		min-width: 31.25em;
 		max-width: 99.75rem;
 		box-sizing: border-box;
 		position: relative;
 		z-index: 0;
-		margin: 0 auto;
-		padding-top: 0.05px;
-		padding-left: 1.5rem;
-		padding-right: 1.5rem;
+		/* margin: 0 auto; */
+		padding-top: 64px;
+		padding-bottom: 64px;
 		background-color: #fff;
 		color: #202122;
 		/* TODO - media queries? */
-		padding-left: 2.75rem;
-		padding-right: 2.75rem;
+		padding-left: 4rem;
+		padding-right: 12rem;
 	}
 
 	.page-title {
 		margin-bottom: 0;
+		margin-top: 0;
 	}
 
 	.page-container-inner {
-		/* TODO */
+		display: grid;
+		column-gap: 24px;
+		grid-template: min-content 1fr min-content / 12.25rem minmax(0, 1fr);
 	}
 
 	.body-content {
@@ -104,8 +159,12 @@
 	.page-title-main {
 	}
 
+	.sider-container {
+		margin-top: 2.8rem;
+	}
+
 	.title-sub {
-		margin-top: 8px;
+		margin: 16px 0px;
 		font-size: 0.875rem;
 	}
 </style>
